149b63b8b4091d66590e3e91fbbb98312483eea4
KennaW <KennaWarsinske@gmail.com>
2015-04-17 11:39:16 -0700

    add html

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,23 @@ var pizzaProcessor = {
   pepperoni: function () {
     this.pizza += 5;
   }
+};
 
-}
+$(document).ready(function () {
+
+  $("form#pizzaSelect").submit(function(event) {
+  event.preventDefault();
+
+  var topping = $("select#toppings").val();
+  var order = Object.Create(pizzaProcessor);
+
+    if (topping==="cheese") {
+      return order.pizza;
+    }
+    elseif(topping==="pepperoni"){
+      order.pepperoni();
+      return order.pizza;
+    }
+
+  });
+});
